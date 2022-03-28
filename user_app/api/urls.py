ec3a86bae7f61361b5ff04e869cb9edf5f485103
sagarvelotio <sagar.hande@velotio.com>
2022-03-28 14:43:03 +0530

    Created logout url

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from user_app.api.views import registration_view
+from user_app.api.views import registration_view, logout_view
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='login'),
-    path('registration/', registration_view, name='registration')
+    path('login/', obtain_auth_token, name="login"),
+    path('registration/', registration_view, name="registration"),
+    path('logout/', logout_view, name="logout")
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,9 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.authtoken.models import Token
 
 from user_app.api.serializers import RegistrationSerializer
+
+
+@api_view(['POST'])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -11,6 +18,12 @@ def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data= request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({"massage": "user created"}, status=status.HTTP_201_CREATED)
-        return Response({"error": "Invalid request data"}, status=status.HTTP_400_BAD_REQUEST)
+            account = serializer.save()
+
+            return Response({"massage": "user created",
+                             "username": account.username,
+                             "email": account.email,
+                             "token": Token.objects.get(user=account).key},
+                            status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors)
