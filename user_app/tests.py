f4695b30199df3a4976247436bc5ddddbdfcf292
Sagar Hande <sagar.hande@velotio.com>
2022-06-06 14:10:20 +0530

    added test cases for user app

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+
+        data = {
+            "username": "testcase",
+            "email": "testcase@example.com",
+            "password": "sample@123",
+            "password2": "sample@123",
+            }
+        response = self.client.post(reverse('registration'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username="testcase", password="test@1234")
+
+    def test_login(self):
+        data = {
+            "username": "testcase",
+            "password": "test@1234",
+            }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username='testcase')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
--- watchmate/settings.py
+++ watchmate/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'watchmate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
